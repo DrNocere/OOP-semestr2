5314211497ab8ae93f22db54996d2d7540633fa6
DrNocere <arturpetrylak@gmail.com>
2022-04-14 19:08:44 +0200

    lab_3 update 04/14/2022b

---
+++ lab_3/FileLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_3
+{
+    public class FileLogger : WriterLogger
+    {
+        private bool disposed;
+        protected FileStream stream;
+
+        public FileLogger(string path)
+        {
+            using (FileStream stream = new FileStream(path, FileMode.Append))
+            {
+                using (TextWriter writer = new StreamWriter(stream, Encoding.UTF8))
+                {
+                    
+                }
+            }
+        }
+
+        ~FileLogger()
+        {
+            this.Dispose(false);
+        }
+
+        public override void Dispose() 
+        {
+            this.disposed = true;
+        }
+    }
+}
