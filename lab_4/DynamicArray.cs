cafb2e9c7e2a99156a06f6e7265d4760a8911100
DrNocere <arturpetrylak@gmail.com>
2022-04-22 16:16:27 +0200

    lab_4 update 04/22/2022

---
+++ lab_4/DynamicArray.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_4
+{
+    internal class DynamicArray<T> : ICollection<T>, IEnumerable<T>
+    {
+        public string Name { get; set; }
+        public T[] Array { get; set; }  
+
+        public int Count { get; set; }
+
+        bool ICollection<T>.IsReadOnly => false;
+
+        public DynamicArray(string name)
+        {
+            Name = name;
+            Array = new T[0];
+        }
+
+        public DynamicArray(string name, int capacity) : this(name)
+        {
+            Array = new T[capacity];
+            Count = capacity;
+        }
+
+        public void Add(T newValue)
+        {
+            Array[Array.Length] = newValue;
+        }
+
+        public void Add(int index, T newValue)
+        {
+            if (Array[index] != null)
+            {
+
+            }
+            Array[index] = newValue;
+            
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            //dynamicArray[index] = null;
+            throw new NotImplementedException();
+        }
+
+        public void Replace(int index, T newValue)
+        {
+            Array[index] = newValue;
+        }
+
+        public T Return(int index)
+        {
+            return Array[index]; 
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ lab_4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
